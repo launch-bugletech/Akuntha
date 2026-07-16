95f46e1425bb1505b0fc349a6588dac305f12ce6
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2026-07-16 19:55:14 +0530

    add: redirect * from /resco-landing-page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/resco-landing-page" element={<App />} />
+        <Route path="*" element={<Navigate to="/resco-landing-page" replace />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
